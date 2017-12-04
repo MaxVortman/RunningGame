3236e6028efbc50ac7353ab475dbdaffee06199f
MaxVortman <maka.abort@gmail.com>
2017-12-04 21:13:35 +0300

    Добавьте файлы проекта.

---
+++ RunningGame/Map.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningGame
+{
+    class Map
+    {
+        #region Map size
+        /// <summary>
+        /// Map Width 
+        /// </summary>
+        public const int Width = 30;
+        /// <summary>
+        /// Map Height
+        /// </summary>
+        public const int Height = 6;
+        #endregion
+
+        #region Public properties
+        /// <summary>
+        /// Map view
+        /// </summary>
+        public char[,] View { get; set; }
+        /// <summary>
+        /// Viewable patterns
+        /// </summary>
+        public List<char[,]> Patterns { get; set; }
+
+        #region Char Constant
+        /// <summary>
+        /// Earth symbol
+        /// </summary>
+        public char[,] Earth = { { ' ' }, { ' ' }, { ' ' }, { ' ' }, { ' ' }, { '#' } };
+        /// <summary>
+        /// Rock view
+        /// </summary>
+        public char[,] SmallRock = { { ' ' }, { ' ' }, { ' ' }, { ' ' }, { '#' }, { '#' } };
+        #endregion
+
+        #endregion
+
+        #region Default constructor
+        public Map()
+        {
+            Patterns = new List<char[,]> { Earth, SmallRock };
+            View = new char[Height, Width];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+
+                    View[i, j] = Earth[i, 0];  
+                }
+            }
+        }
+        #endregion
+        private Random r = new Random();
+        public void Generate()
+        {
+            var newView = new char[Height, Width];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width - 1; j++)
+                {
+                    newView[i, j] = View[i, j + 1];
+                }                                
+            }
+            var randomValue = r.Next() % Patterns.Count;
+            for (int i = 0; i < Height; i++)
+            {
+                newView[i, Width - 1] = Patterns[randomValue][i, 0];
+            }
+            View = newView;
+        }
+
+    }
+}
---
+++ RunningGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace RunningGame
+{
+    class Program
+    {
+        private const int Interval = 100;
+
+        static void Main(string[] args)
+        {
+            var map = new Map();
+            var timer = new Timer();
+            timer.Interval = Interval;
+            timer.Elapsed += (sender, e) =>
+            {
+                Console.Clear();
+                map.Generate();
+                Write(map.View);
+            };
+            timer.Enabled = true;
+            Console.ReadKey();
+        }
+
+        private static void Write(char[,] view)
+        {
+            for (int i = 0; i < Map.Height; i++)
+            {
+                for (int j = 0; j < Map.Width; j++)
+                {
+                    Console.Write(view[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
